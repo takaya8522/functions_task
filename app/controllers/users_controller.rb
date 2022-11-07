7b9f04175db75b2f79a63c04d2928fa53969fdcd
takaya suzuki <suzuki8522@yahoo.co.jp>
2022-11-07 21:19:54 +0900

    action_mailer設定修正
    action_mailer設定修正
    
    action_mailer設定修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      UserMailer.with(to: "test@example.com", name: "dic").welcome.deliver_later(wait: 5.seconds) 
+      UserMailer.with(to: @user.email, name: @user.name).welcome.deliver_later(wait: 5.seconds) 
       log_in(@user)
       redirect_to user_path(@user.id), notice: 'アカウントを登録しました。'
     else
